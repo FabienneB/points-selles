f5e1a039d1a90b044da330c92b15581e7d46626b
Fabienne <fabienne@fidzup.com>
2020-01-15 18:00:41 +0100

    finish kata adding condition to find optimum

--- saddle_points.rb
+++ saddle_points.rb
@@ -1,10 +1,48 @@
+require 'pry'
 class Matrix
-  def initialize(str)
-  
+  def initialize(matrix_str)
+    @matrix = matrix_str.strip.split(/['\n']/).map{ |el| el.split }
+    @matrix = @matrix.each_with_object([]) do |arr, int_matrix|
+      int_matrix << arr.map(&:to_i)
+    end
   end
 
   def rows
-    [[1, 2]]
+    @matrix
   end
-  
+
+  def columns
+    @matrix.transpose
+  end
+
+  def saddle_points
+    # min of column
+    # max of line
+    return [] if @matrix == columns
+    mins = Array.new
+    maxs = Array.new
+    selles_pts = Array.new
+
+    rows.each_with_index do |row, id|
+      # if row.find { |x| row.count(x) > 1 } == row.max
+        # maxs << [row.max, row.index(row.max)]
+        maxs << [row.max, id]
+      else
+        # maxs << [row.max, id]
+      end
+    end
+
+    maxs.each_with_index do |max, id|
+        mins << [columns[max[1]].min, id]
+    end
+    mins.each do |min|
+      maxs.each do |max|
+        begin
+          selles_pts << [max[1], min[1]] if (@matrix[min[1]][max[1]] == min[0] && min[0] == max[0])
+        rescue => exception
+        end
+      end
+    end
+    return selles_pts
+  end
 end
